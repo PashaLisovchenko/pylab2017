d691df66047520347837f5361973f53785197467
PashaLisovchenko <lisovchenko.pasha@gmail.com>
2017-09-09 16:56:11 +0300

    add iterative method

--- dz1_triangle/triangle.py
+++ dz1_triangle/triangle.py
@@ -42,6 +42,37 @@ def chaos_method(my_triangle, my_points):
             my_triangle.dot(DOT_MARK_WIDTH)
     my_triangle.getscreen().mainloop()
 
+def draw_triangle(points, my_turtle):
+    my_turtle.up()
+    my_turtle.goto(points[0][0],points[0][1])
+    my_turtle.down()
+    my_turtle.goto(points[1][0],points[1][1])
+    my_turtle.goto(points[2][0],points[2][1])
+    my_turtle.goto(points[0][0],points[0][1])
+
+
+def get_mid(p1, p2):
+    x = (p1[0] + p2[0]) / 2
+    y = (p1[1] + p2[1]) / 2
+    return x, y
+
+
+def iterative(points, degree, my_turtle):
+    draw_triangle(points, my_turtle)
+    if degree > 0:
+        iterative([points[0],
+                    get_mid(points[0], points[1]),
+                    get_mid(points[0], points[2])],
+                   degree-1, my_turtle)
+        iterative([points[1],
+                    get_mid(points[0], points[1]),
+                    get_mid(points[1], points[2])],
+                   degree-1, my_turtle)
+        iterative([points[2],
+                    get_mid(points[2], points[1]),
+                    get_mid(points[0], points[2])],
+                   degree-1, my_turtle)
+
 
 def sierpinski_triangle(my_points, **kwargs):
     my_triangle = turtle.Turtle()
@@ -49,10 +80,15 @@ def sierpinski_triangle(my_points, **kwargs):
         if kwargs['method'] == 'chaos':
             chaos_method(my_triangle, my_points)
         elif kwargs['method'] == 'iter':
-            pass
-            # iterative(my_triangle, my_points)
+            if 'degree' in kwargs:
+                iterative(my_points, kwargs["degree"], my_triangle)
+            else:
+                iterative(my_points, 2, my_triangle)
     else:
         chaos_method(my_triangle, my_points)
 
-my_points = [[-200, -100], [0, 100], [200, -100]]
-sierpinski_triangle(my_points)
+
+my_points = [[-200, -100], [0, 200], [200, -100]]
+myWin = turtle.Screen()
+sierpinski_triangle(my_points, method="iter")
+myWin.exitonclick()
